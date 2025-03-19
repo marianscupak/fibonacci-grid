f71b9a39943823ed587e34dab02c5893fdf26d0d
Marian Ščupák <scupak.m@gmail.com>
2025-03-19 14:43:15 +0100

    feat: implement highlighting on increment

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,18 @@
 interface Props {
   value: number;
+  backgroundColor?: 'yellow' | 'green';
   onClick(): void;
 }
 
-export const Cell = ({ value, onClick }: Props) => {
+export const Cell = ({ value, backgroundColor, onClick }: Props) => {
+  const bgColorClass =
+    backgroundColor === 'yellow' ? 'bg-yellow-300' : backgroundColor === 'green' ? 'bg-green-300' : 'bg-gray-300';
+
   return (
-    <div className="w-4 h-4 bg-gray-300 border border-gray-400 text-center cursor-pointer" onClick={onClick}>
+    <div
+      className={`w-4 h-4 border border-gray-400 text-center cursor-pointer transition ${bgColorClass}`}
+      onClick={onClick}
+    >
       {value}
     </div>
   );
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import { Cell } from './Cell.tsx';
 
+interface Coordinates {
+  rowIndex: number;
+  cellIndex: number;
+}
+
 interface Props {
   width: number;
   height: number;
 }
 
+const HIGHLIGHT_TIMEOUT = 500;
+
 export const Grid = ({ width, height }: Props) => {
   const [gridState, setGridState] = useState<number[][]>(Array(height).fill(Array(width).fill(1)));
+  const [clickedCell, setClickedCell] = useState<Coordinates | null>(null);
 
   const getOnClickHandler = (rowIndex: number, cellIndex: number) => () => {
     setGridState((prevState) =>
@@ -15,6 +23,9 @@ export const Grid = ({ width, height }: Props) => {
         i === rowIndex ? row.map((cell) => cell + 1) : row.map((cell, j) => (j === cellIndex ? cell + 1 : cell))
       )
     );
+
+    setClickedCell({ rowIndex, cellIndex });
+    setTimeout(() => setClickedCell(null), HIGHLIGHT_TIMEOUT);
   };
 
   return (
@@ -22,7 +33,14 @@ export const Grid = ({ width, height }: Props) => {
       {gridState.map((row, rowIndex) => (
         <div className="flex gap-1" key={rowIndex}>
           {row.map((cellValue, cellIndex) => (
-            <Cell key={`${rowIndex}-${cellIndex}`} value={cellValue} onClick={getOnClickHandler(rowIndex, cellIndex)} />
+            <Cell
+              key={`${rowIndex}-${cellIndex}`}
+              value={cellValue}
+              onClick={getOnClickHandler(rowIndex, cellIndex)}
+              backgroundColor={
+                clickedCell?.rowIndex === rowIndex || clickedCell?.cellIndex === cellIndex ? 'yellow' : undefined
+              }
+            />
           ))}
         </div>
       ))}
