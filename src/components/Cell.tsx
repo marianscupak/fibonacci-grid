ebb106d6afa35893f80cd2cff81e4cff992a7c9f
Marian Ščupák <scupak.m@gmail.com>
2025-03-19 14:09:12 +0100

    feat: implement incrementing values

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,12 @@
 interface Props {
   value: number;
+  onClick(): void;
 }
 
-export const Cell = ({ value }: Props) => {
-  return <div className="w-4 h-4 bg-gray-300 border border-gray-400 text-center">{value}</div>;
+export const Cell = ({ value, onClick }: Props) => {
+  return (
+    <div className="w-4 h-4 bg-gray-300 border border-gray-400 text-center cursor-pointer" onClick={onClick}>
+      {value}
+    </div>
+  );
 };
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -9,12 +9,20 @@ interface Props {
 export const Grid = ({ width, height }: Props) => {
   const [gridState, setGridState] = useState<number[][]>(Array(height).fill(Array(width).fill(1)));
 
+  const getOnClickHandler = (rowIndex: number, cellIndex: number) => () => {
+    setGridState((prevState) =>
+      prevState.map((row, i) =>
+        i === rowIndex ? row.map((cell) => cell + 1) : row.map((cell, j) => (j === cellIndex ? cell + 1 : cell))
+      )
+    );
+  };
+
   return (
     <div className="flex flex-col gap-1">
       {gridState.map((row, rowIndex) => (
         <div className="flex gap-1" key={rowIndex}>
           {row.map((cellValue, cellIndex) => (
-            <Cell key={`${rowIndex}-${cellIndex}`} value={cellValue} />
+            <Cell key={`${rowIndex}-${cellIndex}`} value={cellValue} onClick={getOnClickHandler(rowIndex, cellIndex)} />
           ))}
         </div>
       ))}
